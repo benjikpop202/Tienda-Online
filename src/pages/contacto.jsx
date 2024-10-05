81eeee3b394fbbc63674417b049ca9da06c261c6
benjikpop202 <benjaminsobarzo2018@gmail.com>
2024-10-05 11:08:27 -0300

    agregando card de contacto

--- src/pages/contacto.jsx
+++ src/pages/contacto.jsx
@@ -4,6 +4,10 @@ const Contacto = ()=>{
     return(
        <div className="ContactoContainer">
           <div className="CardContact">
+            <h1>Seguinos en Nuestras Redes</h1>
+          <div className='raw'><img src="/assets/products/facebook (2).png" alt="facebook" /><h2>Carrito Express</h2></div>
+          <div className="raw"><img src="/assets/products/instagram (2).png" alt="instagram" /><h2>Carrito_express</h2></div>
+          <div className="raw"> <img src="/assets/products/twitter-alt.png" alt="twitter" /><h2>@CarritoExpress</h2></div>
           </div>
        </div>
     )
